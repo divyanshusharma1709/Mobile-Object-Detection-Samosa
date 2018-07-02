6e29a27e1c34dba2c800beb6b10998c311509b78
Divyanshu Sharma <sharmadivyanshu1709@gmail.com>
2018-07-02 11:48:32 +0530

    1. TODO: Train using one image or use complete datasaet

--- convLayerFn.py
+++ convLayerFn.py
@@ -7,7 +7,6 @@ Created on Sat Jun 23 12:13:14 2018
 """
 import tensorflow as tf
 import numpy as np
-
 tf.reset_default_graph()
 import pickle
 def getImage(start, end):
@@ -23,10 +22,27 @@ def getImage(start, end):
             labels.append(dictname[1][i])
     return images, labels, (end - start + 1)
 
-features, labels, num = getImage(0,5)
+features, labels, num = getImage(0,96)
+labels = np.array(labels, dtype=np.int32)
 n_classes = 1
-x = tf.placeholder("float", [None, 256,256,3])
-y = tf.placeholder("float", [5])
+x = tf.placeholder(tf.float32, [None, 256,256,3])
+y = tf.placeholder(tf.int32, [5])
+
+def neuron_layer(X, n_neurons, n_inputs, name, activation = None):
+    stddev = 2/np.sqrt(n_inputs)
+    init = tf.truncated_normal((n_inputs, n_neurons), stddev = stddev)
+    W = tf.Variable(init, name = "Weights")
+    b = tf.Variable(tf.zeros([n_neurons]), name = "Biases")
+    z = tf.matmul(X,W) + b
+    if activation == "relu":
+        return tf.nn.relu(z)
+    else:
+        return z
+
+
+
+
+
 
 def conv_layer(input_data, weights, bias, strides):
     x = tf.nn.conv2d(input_data, weights, strides, padding='SAME')
@@ -42,15 +58,11 @@ weights = {
     'wc1': tf.get_variable('W0', shape=(3,3,3,32), initializer=tf.contrib.layers.xavier_initializer()), 
     'wc2': tf.get_variable('W1', shape=(3,3,32,64), initializer=tf.contrib.layers.xavier_initializer()), 
     'wc3': tf.get_variable('W2', shape=(3,3,64,128), initializer=tf.contrib.layers.xavier_initializer()), 
-    'wd1': tf.get_variable('W3', shape=(4*4*128,128), initializer=tf.contrib.layers.xavier_initializer()), 
-    'out': tf.get_variable('W6', shape=(128,n_classes), initializer=tf.contrib.layers.xavier_initializer()), 
 }
 bias = {
     'bc1': tf.get_variable('B0', shape=(32), initializer=tf.contrib.layers.xavier_initializer()),
     'bc2': tf.get_variable('B1', shape=(64), initializer=tf.contrib.layers.xavier_initializer()),
     'bc3': tf.get_variable('B2', shape=(128), initializer=tf.contrib.layers.xavier_initializer()),
-    'bd1': tf.get_variable('B3', shape=(128), initializer=tf.contrib.layers.xavier_initializer()),
-    'out': tf.get_variable('B4', shape=(2), initializer=tf.contrib.layers.xavier_initializer()),
 }
 
 
@@ -64,29 +76,32 @@ pool1 = tf.nn.relu(pool1)
 conv3 = conv_layer(pool1, weights['wc3'], bias['bc3'],strides = [1,2,2,1])
 pool2 = max_pool_layer(conv3,[4,4],[1,2,2,1])
 pool2 = tf.nn.relu(pool2)
-init = tf.global_variables_initializer()
 #Fully connected layer
-fc1 = tf.reshape(pool2, [-1, weights['wd1'].get_shape().as_list()[0]])
-fc1 = tf.add(tf.matmul(fc1, weights['wd1']), bias['bd1'])
+fc1 = tf.reshape(pool2, [1, -1])
 fc1 = tf.nn.relu(fc1)
+fc2 = neuron_layer(fc1, 100,40960, name = "FCL2", activation = "relu")
 
-out = tf.add(tf.matmul(fc1, weights['out']), bias['out'])
-cost = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(labels = [0,1], logits = [0,1]))
-optimizer = tf.train.GradientDescentOptimizer(learning_rate = 0.001).minimize(cost)
-correct_prediction = tf.equal(tf.argmax(out, 1), tf.argmax(y, 1))
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+output = neuron_layer(fc2, 1,100, name = "Output", activation = "relu")
+
+
+xentropy = tf.nn.sparse_softmax_cross_entropy_with_logits(labels = y, logits = output)
+loss = tf.reduce_mean(xentropy, name = "Loss")
+optimizer = tf.train.GradientDescentOptimizer(learning_rate = 0.01)
+training_op = optimizer.minimize(loss)
+correct = tf.nn.in_top_k(output, y, 1)
+accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
+
+init = tf.global_variables_initializer()
 
 
 
 with tf.Session() as sess:
     sess.run(init)
-    train_loss = []
-    test_loss = []
-    train_accuracy = []
-    test_accuracy = []
-    summary_writer = tf.summary.FileWriter('./Output', sess.graph)
-    opt = sess.run(optimizer, feed_dict={x: features, y: labels})
-    loss = sess.run(cost, feed_dict={x: features, y: labels})
-    print(loss)
-    print("Optimization Finished!")
-    print(weights)
+#    out = sess.run(pool2, feed_dict = {x: features})
+    for epoch in range(400):
+        for iteration in range(5):
+            features, labels = 
+        
+    out = sess.run(output, feed_dict = {x: features, y:labels})
+    
+   
